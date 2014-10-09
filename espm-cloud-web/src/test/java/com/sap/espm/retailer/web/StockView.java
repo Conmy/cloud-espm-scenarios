0e08681b63d4c8140e78a3f0cb8af5be52866eeb
KUMARANKU <ankur.kumar01@sap.com>
2014-10-09 16:29:04 +0530

    Update selenium versions and remove usage of internal libraries

--- espm-cloud-web/src/test/java/com/sap/espm/retailer/web/StockView.java
+++ espm-cloud-web/src/test/java/com/sap/espm/retailer/web/StockView.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.internal.seleniumemulation.JavascriptLibrary;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import com.thoughtworks.selenium.webdriven.JavascriptLibrary;
 
 public class StockView {
 
