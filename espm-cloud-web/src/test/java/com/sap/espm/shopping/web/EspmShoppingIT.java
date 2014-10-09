02976ebcac30fa99c2b6718c836c44650c31feb8
KUMARANKU <ankur.kumar01@sap.com>
2014-10-09 16:12:03 +0530

    Update selenium versions and remove usage of internal libraries

--- espm-cloud-web/src/test/java/com/sap/espm/shopping/web/EspmShoppingIT.java
+++ espm-cloud-web/src/test/java/com/sap/espm/shopping/web/EspmShoppingIT.java
@@ -23,11 +23,12 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.internal.seleniumemulation.JavascriptLibrary;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.thoughtworks.selenium.webdriven.JavascriptLibrary;
 
 public class EspmShoppingIT {
 
