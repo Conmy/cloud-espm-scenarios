2c0ff098164ec292d250941c59a3abdc7051c583
KUMARANKU <ankur.kumar01@sap.com>
2014-10-09 14:53:47 +0530

    Update selenium versions and remove usage of internal libraries

--- espm-cloud-web/src/test/java/com/sap/espm/retailer/web/SalesOrderView.java
+++ espm-cloud-web/src/test/java/com/sap/espm/retailer/web/SalesOrderView.java
@@ -3,9 +3,10 @@ package com.sap.espm.retailer.web;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.internal.seleniumemulation.JavascriptLibrary;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import com.thoughtworks.selenium.webdriven.JavascriptLibrary;
 
 public class SalesOrderView {
 
